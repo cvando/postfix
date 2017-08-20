4279be12871feb6ed86dad7ce40dc9b552a088c2
cvando <clement.vandoolaeghe@gmail.com>
2017-08-20 20:17:03 +0200

    startup tail *.log

--- startup.sh
+++ startup.sh
@@ -6,11 +6,10 @@ chown -R $1:mail /var/spool/mail/$1
 chmod -R a=rwx /var/spool/mail/$1
 chmod -R o=- /var/spool/mail/$1
 touch /var/log/mail.log
-
 sed -i "s/myhostname = server1.example.com/myhostname = $3/g" /etc/postfix/main.cf
 
 service syslog-ng start
 service saslauthd start
 service postfix start
 
-tail -F /var/log/mail.log
+tail -F /var/log/mail.*
