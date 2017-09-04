f57776c780f21713259b2977a531b23906e62176
cvando <clement.vandoolaeghe@gmail.com>
2017-09-04 16:10:26 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,17 +8,6 @@ chmod -R o=- /var/spool/mail/$1
 touch /var/log/mail.log
 sed -i "s/myhostname = server1.example.com/myhostname = $3/g" /etc/postfix/main.cf
 
-
-cd /etc/postfix/ssl/
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out smtpd.key
-rm server.pass.key
-openssl req -new -key smtpd.key -out server.csr \
-  -subj "/C=FR/ST=IDF/L=paris/O=org/OU=ou/CN=postfix"
-openssl x509 -req -days 3650 -in server.csr -signkey smtpd.key -out smtpd.crt
-cd /
-
-
 service syslog-ng start
 service saslauthd start
 service postfix start
